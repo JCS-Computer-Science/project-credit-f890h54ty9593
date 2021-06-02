7b04d408576a72de3d3905a483dac90a5cfc68b0
Dominic Castor <904162011@edu.cbe.ab.ca>
2021-06-02 12:32:56 -0600

    .js comments

--- js/products.js
+++ js/products.js
@@ -1,6 +1,8 @@
+//variables for getting text id of item
 var ul = document.getElementById("bag1");
 var li = document.querySelectorAll("li");
 
+//function for text child on image, when clicked add to ul
 function add(id){
     var li_new = document.createElement("li");
     var li_inp = document.createTextNode(id);
@@ -9,6 +11,7 @@ function add(id){
     ul.appendChild(document.createElement("br"));
 }
 
+//clears ul
 window.emptyList = function () {
     var ul = document.querySelector('#bag1');
     var listLength = ul.children.length;
